98cac2ba7550830f5baf4ad298242d0729cbc3af
sming <1534441796@qq.com>
2023-05-29 20:52:03 +0800

    创建

--- git/helloGit.idea/src/main/java/com/git/hello.java
+++ git/helloGit.idea/src/main/java/com/git/hello.java
@@ -1,8 +1,25 @@
 package com.git;
 
+import java.util.*;
+
 public class hello {
      public static void main(String[] args) {
         System.out.println("Hello Git");
-         System.out.println("创建");
-    }
+        System.out.println("创建");
+        List<Integer> list = new ArrayList<Integer>();
+        Map<String,Integer> hmap = new HashMap();
+        hmap.put("b",3);
+        hmap.put("bz",32);
+        hmap.put("a",4);
+        Set<Map.Entry<String,Integer>> entyrSet = hmap.entrySet();
+        Iterator<Map.Entry<String, Integer>> iterator = entyrSet.iterator();
+        while(iterator.hasNext()){
+             Map.Entry<String,Integer> entry = iterator.next();
+             String key = entry.getKey();
+             Integer ite = entry.getValue();
+             System.out.println(key+"---"+ite);
+
+         }
+
+     }
 }
